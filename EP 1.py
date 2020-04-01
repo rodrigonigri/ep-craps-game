60a4adb2651c3158c36c0ce1fd6c90fb74fe7f79
Rodrigo Nigri Griner <rodrigong1@al.insper.edu.br>
2020-04-01 19:25:00 -0300

    Arrumei os detalhes finais do jogo

--- EP 1.py
+++ EP 1.py
@@ -1,16 +1,16 @@
 from random import randint
 Fichas_iniciais=1000
-print("Fichas iniciais: {0}".format(Fichas_iniciais))
 a = True
 b = True
 print('Faça sua aposta e decida quanto você quer apostar. As possíveis apostas são: Pass_line, Field, Any_craps e Twelve')
-aposta = input("Escolha uma aposta dentre as 4 acima: ")
-quantidade=int(input("Quanto você quer apostar: "))
-while b:
+while b and Fichas_iniciais>0:
     pergunta=input('Você quer continuar?(sim/não): ')
     if pergunta=="não":
         b = False
     else:
+        aposta = input("Escolha uma aposta dentre as 4(Pass_line, Field, Any_craps e Twelve): ")
+        print("Fichas iniciais: {0}".format(Fichas_iniciais))
+        quantidade=int(input("Quanto você quer apostar???(OBS:. Para jogar honestamente, coloque um valor menor ou igual a sua quantidade atual): "))
         dado_1 = randint(1,6)
         dado_2 = randint(1,6)
         if aposta == "Pass_line":
@@ -19,20 +19,17 @@ while b:
                 print("Os dados foram {0} e {1} somando {2}".format(dado_1,dado_2,dado_1+dado_2))
                 print("PARABENS, VOCÊ GANHOU")
                 Fichas_iniciais = Fichas_iniciais+quantidade
-                b=False
                 print("Sua quantidade de fichas atual é {0}".format(Fichas_iniciais))
             elif dado_1+dado_2 == 2 or dado_1+dado_2 == 3 or dado_1+dado_2 == 12:
                 print("Os dados foram {0} e {1} somando {2}".format(dado_1,dado_2,dado_1+dado_2))
                 Fichas_iniciais-= quantidade
                 print("NÃO FOI DESSA VEZ")
                 print("Sua quantidade de fichas atual é {0}".format(Fichas_iniciais))
-                b=False
             else:
                 print("Os dados foram {0} e {1} somando {2}".format(dado_1,dado_2,dado_1+dado_2))
                 print('Você foi para a FASE POINT')
                 point=dado_1+dado_2
                 print("O seu Point é {0}".format(point))
-                b=False
                 while a:
                     dado_3=randint(1,6)
                     dado_4=randint(1,6)
@@ -95,3 +92,4 @@ while b:
                 print("NÃO FOI DESSA VEZ")
                 Fichas_iniciais-=quantidade
                 print(Fichas_iniciais)
+print("Você terminou o jogo com {0} fichas".format(Fichas_iniciais))
