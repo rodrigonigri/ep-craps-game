b29846269d71d32e878c7c5e183c17f8df4159c0
Rodrigo Nigri Griner <rodrigong1@al.insper.edu.br>
2020-04-03 11:37:19 -0300

    Finalização do código

--- EP 1.py
+++ EP 1.py
@@ -1,7 +1,7 @@
 from random import randint
 Fichas_iniciais=1000
-a = True
-b = True
+a = True #É a trava do while na fase Point
+b = True #É a trava do while na fase Come out
 print('Faça sua aposta e decida quanto você quer apostar. As possíveis apostas são: Pass_line, Field, Any_craps e Twelve')
 while b and Fichas_iniciais>0:
     pergunta=input('Você quer continuar?(sim/não): ')
@@ -47,49 +47,46 @@ while b and Fichas_iniciais>0:
                     else:
                         a=True
         elif aposta == "Field":
-            #print("Você está na fase C")
             if dado_1+dado_2==5 or dado_1+dado_2==6 or dado_1+dado_2==7 or dado_1+dado_2==8:
                 print("Os dados foram {0} e {1}, somando {2}".format(dado_1,dado_2,dado_1+dado_2))
                 print("NÃO FOI DESSA VEZ")
                 Fichas_iniciais-=quantidade
-                print(Fichas_iniciais)
+                print("Sua quantidade de fichas atual é {0}".format(Fichas_iniciais))
             elif dado_1+dado_2==3 or dado_1+dado_2==4 or dado_1+dado_2==9 or dado_1+dado_2==10 or dado_1+dado_2==11:
                 print("Os dados foram {0} e {1}, somando {2}".format(dado_1,dado_2,dado_1+dado_2))
                 print("PARABÉNS, VOCÊ GANHOU")
                 Fichas_iniciais+=quantidade
-                print(Fichas_iniciais)
+                print("Sua quantidade de fichas atual é {0}".format(Fichas_iniciais))
             elif dado_1+dado_2==2:
                 print("Os dados foram {0} e {1}, somando {2}".format(dado_1,dado_2,dado_1+dado_2))
                 print("PARABÉNS,VOCÊ GANHOU EM DOBRO")
                 Fichas_iniciais+=2*quantidade
-                print(Fichas_iniciais)
+                print("Sua quantidade de fichas atual é {0}".format(Fichas_iniciais))
             else:
                 print("Os dados foram {0} e {1}, somando {2}".format(dado_1,dado_2,dado_1+dado_2))
                 print("PARABÉNS, VOCÊ GANHOU 3x O VALOR!!!")
                 Fichas_iniciais+=3*quantidade
-                print(Fichas_iniciais)
+                print("Sua quantidade de fichas atual é {0}".format(Fichas_iniciais))
         elif aposta == 'Any_craps':
-            #print("Você está na fase C")
             if dado_1+dado_2==2 or dado_1+dado_2==3 or dado_1+dado_2==12:
                 print("Os dados foram {0} e {1}, somando {2}".format(dado_1,dado_2,dado_1+dado_2))
                 print("PARABÉNS, VOCÊ GANHOU 7x O VALOR!!!")
                 Fichas_iniciais+=7*quantidade
-                print(Fichas_iniciais)
+                print("Sua quantidade de fichas atual é {0}".format(Fichas_iniciais))
             else:
                 print("Os dados foram {0} e {1}, somando {2}".format(dado_1,dado_2,dado_1+dado_2))
                 print("NÃO FOI DESSA VEZ")
                 Fichas_iniciais-=quantidade
-                print(Fichas_iniciais)
+                print("Sua quantidade de fichas atual é {0}".format(Fichas_iniciais))
         elif aposta == "Twelve":
-            #print("Você está na fase C")
             if dado_1+dado_2 == 12:
                 print("Os dados foram {0} e {1}, somando {2}".format(dado_1,dado_2,dado_1+dado_2))
                 print("PARABÉNS, VOCÊ GANHOU 30x O VALOR!!!")
                 Fichas_iniciais+=30*quantidade
-                print(Fichas_iniciais)
+                print("Sua quantidade de fichas atual é {0}".format(Fichas_iniciais))
             else:
                 print("Os dados foram {0} e {1}, somando {2}".format(dado_1,dado_2,dado_1+dado_2))
                 print("NÃO FOI DESSA VEZ")
                 Fichas_iniciais-=quantidade
-                print(Fichas_iniciais)
+                print("Sua quantidade de fichas atual é {0}".format(Fichas_iniciais))
 print("Você terminou o jogo com {0} fichas".format(Fichas_iniciais))
